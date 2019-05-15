aa091067e20222cc50bb241a60a18f679d38cd42
Warren Yao <warrenyca905@gmail.com>
2019-05-15 00:09:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if word.match(/\b/)
+  if word.match(/\b[A-Z]/)
 end
 
 def valid_phone_number?(phone)
