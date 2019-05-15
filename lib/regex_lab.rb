8746d9c70ffd09ac3b4bc3bfddb3ca2d32c84c98
Warren Yao <warrenyca905@gmail.com>
2019-05-15 02:21:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\b[A-Z]\w+[!,.?]/)
+  phone_number = phone.match(/(\d{3})\D*(\d{3})\D*(\d{4})\b/)
+  if phone_number
     return true
   else
     false
