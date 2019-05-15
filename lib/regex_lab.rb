cf3c7d85025e282aec1039ef9f96dc4236d8ddf5
Warren Yao <warrenyca905@gmail.com>
2019-05-15 01:27:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\b[A-Z]\w+\W/)
+  if text.match(/[A-Z]\w+\W/)
     return true
   else
     false
